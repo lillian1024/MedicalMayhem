03f455df26f3ac1b806bb22867d304558beeb4b5
Lilian Labat <lilian.labat@epita.fr>
2025-01-29 14:52:52 -0800

    feat: added debug printf in main.c:main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include "config.h"
+
 int main(int argc, char* argv[])
 {
-    
+    printf("Hello from %s %d.%d\n", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR);
 
     return 0;
 }
